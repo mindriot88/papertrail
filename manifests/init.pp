1113ca053953fd8b69dd6485e998737b747a99ab
Jeffrey Schilperoord <jeffrey.schilperoord@tangelo.nl>
2015-03-23 20:36:02 +0100

    Explained working parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,13 +2,22 @@
 #
 # This module manages papertrail
 #
-# Parameters: none
+# Parameters: 
+#
+# [*log_port*]
+#  The port number assigned by papertrail. Should reflect your the port in your account
+#  There is no default as it should be changed in any situation
+#
+# [*log_host*]
+#  The host assigned by papertrail. Should reflect your the host in your account
+#  There is no default as it should be changed in any situation
+#
+# [*extra_logs*]
+#  Array with log files to push to papertrail. Also supports wildcards
 #
 # Actions:
 #
 # Requires: see Modulefile
-#
-# Sample Usage:
 #
 class papertrail(
   $log_port               = $papertrail::params::log_port,
